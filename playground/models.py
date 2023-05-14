1c92ed8c9a62806b9e60bbfdd3ab8f63f0691c69
Artyom <arkhamb@yandex.ru>
2023-05-15 02:44:51 +0300

    общий коммит для удаление мусорных файлов и обычного обновления

--- playground/models.py
+++ playground/models.py
@@ -13,23 +13,24 @@ class TEST_Like_films(models.Model):
 class Cinemas(models.Model):
     name = models.CharField(max_length=20, null=True)
     page = models.TextField(null=True)
-    id_cinema = models.IntegerField(primary_key=True)
+    id_cinema = models.AutoField(primary_key=True)
     photo = models.ImageField(null=True)
 
 class Films(models.Model):
-    name = models.CharField(max_length=70, null=True)
+    film_name = models.CharField(max_length=70, null=True)
     link = models.URLField(null=True)
     # description = models.TextField(null=True)
-    id_film = models.IntegerField(primary_key=True)
+    id_film = models.AutoField(primary_key=True)
     photo = models.URLField(null=True)
 
-class Films_cost(models.Model):
-    purchase = models.DecimalField(max_digits=19, decimal_places=4, null=True)
-    rental = models.DecimalField(max_digits=19, decimal_places=4, null=True)
+class Films_Cost(models.Model):
+    viewing_method = models.CharField(max_length=20, null=True)
+    quality = models.CharField(max_length=15, null=True)
+    cost = models.DecimalField(max_digits=19, decimal_places=4, null=True)
     id_filmRequest = models.IntegerField(primary_key=True)
     id_film = models.ForeignKey(Films, null=True, on_delete=models.SET_NULL)
     id_cinema = models.ForeignKey(Cinemas, null=True, on_delete=models.SET_NULL)
 
 class Like_films(models.Model):
-    id_filmRequest = models.ForeignKey(Films_cost, on_delete=models.CASCADE)
+    id_filmRequest = models.ForeignKey(Films_Cost, on_delete=models.CASCADE)
     id_user = models.ForeignKey('registration.Profile', on_delete=models.CASCADE)
--- playground/urls.py
+++ playground/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('delete/<int:id>/', views.delete, name='delete'),
     path('update/<int:id>/', views.update, name='update'),
-    path('about_us/', views.wir, name='about us')
+    path('about_us/', views.wir, name='about us'),
+    path('movie_results/', views.search_movie, name='movie results')
+
 ]
